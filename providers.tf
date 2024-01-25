3e915535c067e2b0b52b05fb872bbe794405cd19
obewMEH <obe.wullems@mosdex-ehealth.nl>
2024-01-25 11:48:26 +0100

    haakje verkeerd oeps

--- providers.tf
+++ providers.tf
@@ -5,11 +5,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
-  }
+  
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
+  }
 
     backend "azurerm" {
     resource_group_name  = "terraform-github-actions"
