990abbd2a42e4df62877d98ae4dea679a5d809f9
obewMEH <obe.wullems@mosdex-ehealth.nl>
2024-02-01 14:31:16 +0100

    revert oidc change

--- providers.tf
+++ providers.tf
@@ -14,12 +14,12 @@ terraform {
     storage_account_name = "terraformgithubpoc"
     container_name       = "tfstatefile"
     key                  = "state"
-    use_oidc             = false
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = false
+  use_oidc = true
 
 }
