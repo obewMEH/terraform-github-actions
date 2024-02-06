d9bcb5532506da26f9fa42251378d35065e85c4f
obewMEH <154326040+obewMEH@users.noreply.github.com>
2024-02-06 10:49:19 +0100

    syntax fout extra bracket

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "resource_group_location" {
   default     = "westeurope"
   description = "Location of the resource group."
 }
-
+}
 variable "resource_group_name_prefix" {
   type        = string
   default     = "rg"
