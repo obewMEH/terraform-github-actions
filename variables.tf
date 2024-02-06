dbe8e81525296f9ef638a336b3c14d298243948b
obewMEH <154326040+obewMEH@users.noreply.github.com>
2024-02-06 10:56:41 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,6 @@ variable "resource_group_location" {
   default     = "westeurope"
   description = "Location of the resource group."
 }
-}
 variable "resource_group_name_prefix" {
   type        = string
   default     = "rg"
